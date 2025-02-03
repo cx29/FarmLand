7b5aea016b234a2f7b14082463c8d02f5d2116c2
twonine <1185366392@qq.com>
2025-02-04 00:32:40 +0800

    添加碰撞体边界

--- Farmland/Assets/Scripts/Player/Player.cs
+++ Farmland/Assets/Scripts/Player/Player.cs
@@ -27,12 +27,17 @@ public class Player : MonoBehaviour
    {
       float horizontal = Input.GetAxis("Horizontal");
       float vertical = Input.GetAxis("Vertical");
-      movement = horizontal != 0 || vertical != 0 ? new Vector2(horizontal, vertical).normalized : Vector2.zero;
+      //只有在斜向移动的时候才进行归一化计算，减小垂直水平行走时不必要的计算开销
+      movement=new Vector2(horizontal,vertical);
+      if (movement.sqrMagnitude > 1)
+      {
+         movement.Normalize();
+      }
    }
 
    private void Move()
    {
       var distance = _rigidbody2D.position + movement * (_speed * Time.deltaTime);
-      _rigidbody2D.MovePosition(distance);
+      _rigidbody2D.MovePosition(Vector2.Lerp(_rigidbody2D.position, distance, 0.5f));
    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FrameLand
+
+## Grid Layer
+- Ground Bottom     地面最底层
+- Ground Middle     地面中间层
+- Ground Top        地面最顶层
+- Stuff 1           地面装饰层
+- Stuff 2           地面装饰层
+- Dig               挖坑图层
+- Water             浇水涂层
+- Instance          角色涂层
+- Front 1           遮挡层
+- Front 2           遮挡层
+- Collision         边缘碰撞层
+- Grid Properties   网格的属性，用来标识行为可行区域
+    - CanDig            可以挖土
+    - CanDropItem       可以扔弃
+    - CanPlaceFurniture 可以摆放家具
+    - NPC Obstacle      NPC障碍物
